bff0ea89e00e9a94d07a3760b6fdfe417d7af281
Lily Silva <lilys.prog@gmail.com>
2022-10-18 13:09:40 -0300

    DOM studies

---
+++ ModuleB/exemplosJS.js
@@ -0,0 +1,10 @@
+let s = 'Javascript';
+console.log('Eu estou estudando ' + s);
+
+//=============================================
+
+let nome = 'Gustavo';
+let idade = 41;
+let nota = 5.5;
+// console.log('O aluno ' + nome + ', de ' + idade + ' anos, tirou a nota ' + nota);
+console.log(`O aluno ${nome}, de ${idade} anos, tirou a nota ${nota}`);
