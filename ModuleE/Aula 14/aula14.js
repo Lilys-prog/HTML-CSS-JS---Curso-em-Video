4898404a734b46251fa18ee9707fd52912f63919
Lily Silva <lilys.prog@gmail.com>
2022-10-25 19:56:54 -0300

    algumas integrações entre js e html

---
+++ ModuleE/Aula 14/aula14.js
@@ -0,0 +1,21 @@
+// usando while
+let c = 1
+while (c <= 5) {
+  console.log(c)
+  c++
+}
+
+// usando do/while
+let cont = 1
+do {
+  console.log(cont);
+  cont++;
+} while (cont <= 5);
+
+// usando for e testando o depurador
+console.log('Vai começar...');
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log('Terminou!');
+// for é o mais usado de todos
