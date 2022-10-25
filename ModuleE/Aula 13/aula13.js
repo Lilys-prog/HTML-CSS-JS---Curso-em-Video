947baa3ff1e91befcefb8f10fbafda00c3caa243
Lily Silva <lilys.prog@gmail.com>
2022-10-25 18:04:02 -0300

    estruturas de repetição

---
+++ ModuleE/Aula 13/aula13.js
@@ -0,0 +1,23 @@
+// console.log('Tudo bem? ');
+// console.log('Tudo bem? ');
+// console.log('Tudo bem? ');
+// console.log('Tudo bem? ');
+// console.log('Tudo bem? ');
+// console.log('Tudo bem? ');
+// Ao invés de fazer isso:
+
+
+// estrutura while/'do' (o 'do' é ímplicito)
+let cont = 1;
+while (cont <= 6) {
+  console.log('Tudo bem? ');
+  cont++;
+}
+
+
+// estrutura do/while
+let c = 1;
+do {
+  console.log('Tudo bem?');
+  c++;
+} while (c <= 6);
