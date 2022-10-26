1fbebb1521310fe349d499f7f8462d0d01e84cb9
Lily Silva <lilys.prog@gmail.com>
2022-10-26 11:39:19 -0300

    exercício de repetição 1: contador

---
+++ ModuleE/aula 15 exerc/ex001/script.js
@@ -0,0 +1,9 @@
+inicio = document.getElementById('inicio');
+fim = document.getElementById('fim');
+salto = document.getElementById('salto')
+btn = document.getElementById('btn');
+cont = document.getElementById('cont');
+
+function contador() {
+  
+}
