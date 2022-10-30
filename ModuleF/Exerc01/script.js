004ae2b85a26779eeaab6c834875c786a6af1d00
Lily Silva <lilys.prog@gmail.com>
2022-10-30 13:23:06 -0300

    primeiro dos últimos exercícios

---
+++ ModuleF/Exerc01/script.js
@@ -0,0 +1,7 @@
+/*
+- solicitar o input de um número de 1 a 100 e um botão na mesma linha escrito 'Adicionar'
+- adicionar um input select para receber os números adicionados
+- adicionar um botão que vai realizar operações com os números adicionados
+
+*/
+
